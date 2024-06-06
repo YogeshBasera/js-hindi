da3b2ef523ba79d2597f8e0e58e095d307225105
YogeshBasera <yogeshbasera999@gmail.com>
2024-06-06 12:59:48 +0530

    Update text.js

--- .devcontainer/01_basics/text.js
+++ .devcontainer/01_basics/text.js
@@ -1,1 +1,6 @@
 console.log("Hello Yogesh")
+const FullName = "Yogesh Basera"
+const Age = 21
+const EmailId = "yogeshbasera999@gmail.com"
+const MobileNum = 7900668347
+console.log([FullName, Age, EmailId, MobileNum]);
